7bacfa267038b12eb3bb7a880f7ab98e52b9d38b
a-kosuge <a-kosuge@atware.co.jp>
2020-04-19 00:00:02 +0900

    add show status code

--- ch01/ex09/ch01-ex09.go
+++ ch01/ex09/ch01-ex09.go
@@ -18,6 +18,7 @@ func main() {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
 		}
+		fmt.Printf("status code: %d\n", resp.StatusCode)
 		_, err = io.Copy(os.Stdout, resp.Body)
 		resp.Body.Close()
 		if err != nil {
