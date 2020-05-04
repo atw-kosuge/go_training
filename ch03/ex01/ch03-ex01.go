9b4c5833973dc273abb442b87b0e7c68443193bb
a-kosuge <a-kosuge@atware.co.jp>
2020-05-04 21:57:06 +0900

    skip NaN

--- ch03/ex01/ch03-ex01.go
+++ ch03/ex01/ch03-ex01.go
@@ -8,6 +8,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"math"
 )
@@ -29,29 +30,34 @@ func main() {
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
-				ax, ay, bx, by, cx, cy, dx, dy)
+			ax, ay, aerr := corner(i+1, j)
+			bx, by, berr := corner(i, j)
+			cx, cy, cerr := corner(i, j+1)
+			dx, dy, derr := corner(i+1, j+1)
+			if aerr == nil && berr == nil && cerr == nil && derr == nil {
+				fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+					ax, ay, bx, by, cx, cy, dx, dy)
+			}
 		}
 	}
 	fmt.Println("</svg>")
 }
 
-func corner(i, j int) (float64, float64) {
+func corner(i, j int) (float64, float64, error) {
 	// Find point (x,y) at corner of cell (i,j).
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 
 	// Compute surface height z.
 	z := f(x, y)
+	if math.IsNaN(z) {
+		return 0, 0, errors.New("f is NaN")
+	}
 
 	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
-	return sx, sy
+	return sx, sy, nil
 }
 
 func f(x, y float64) float64 {
