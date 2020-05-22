62483d786d464a1aa75e0b79fe57683663928a44
a-kosuge <a-kosuge@atware.co.jp>
2020-05-22 16:09:39 +0900

    remove access token

--- ch04/ex11/create_issue.sh
+++ ch04/ex11/create_issue.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-go run ch04-ex11.go create "8f3ddf2ff691da2af2032709fc16dc0222ddd5d5" "atware-a-kosuge" "go_training" abc def
+go run ch04-ex11.go create [AccessToken] "atware-a-kosuge" "go_training" abc def
