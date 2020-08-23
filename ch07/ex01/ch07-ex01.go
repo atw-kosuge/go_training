ff84536bdd8eb503323d0b86fa626cffc84298b1
a-kosuge <a-kosuge@atware.co.jp>
2020-08-23 11:30:13 +0900

    add WordCounter and LineCounter

---
+++ ch07/ex01/ch07-ex01.go
@@ -0,0 +1,70 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 173.
+
+// Bytecounter demonstrates an implementation of io.Writer that counts bytes.
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+)
+
+// ByteCounter バイトカウンタ
+type ByteCounter int
+
+func (c *ByteCounter) Write(p []byte) (int, error) {
+	*c += ByteCounter(len(p)) // convert int to ByteCounter
+	return len(p), nil
+}
+
+// LineCounter 行カウンタ
+type LineCounter int
+
+func (c *LineCounter) Write(p []byte) (int, error) {
+	scanner := bufio.NewScanner(bytes.NewReader(p))
+	scanner.Split(bufio.ScanLines)
+	count := 0
+	for scanner.Scan() {
+		count++
+	}
+	*c += LineCounter(count)
+	return count, scanner.Err()
+}
+
+// WordCounter Wordカウンタ
+type WordCounter int
+
+func (c *WordCounter) Write(p []byte) (int, error) {
+	scanner := bufio.NewScanner(bytes.NewReader(p))
+	scanner.Split(bufio.ScanWords)
+	count := 0
+	for scanner.Scan() {
+		count++
+	}
+	*c += WordCounter(count)
+	return count, scanner.Err()
+}
+
+func main() {
+	//!+main
+	var c ByteCounter
+	c.Write([]byte("hello"))
+	fmt.Println(c) // "5", = len("hello")
+
+	c = 0 // reset the counter
+	var name = "Dolly"
+	fmt.Fprintf(&c, "hello, %s", name)
+	fmt.Println(c) // "12", = len("hello, Dolly")
+	//!-main
+
+	var l LineCounter
+	l.Write([]byte("This is\na\npen"))
+	fmt.Println(l)
+
+	var w WordCounter
+	w.Write([]byte("This is a pen"))
+	fmt.Println(w)
+}
